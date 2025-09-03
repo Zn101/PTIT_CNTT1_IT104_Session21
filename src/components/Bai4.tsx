85711893c4eec51edb34fa8b848914e09b15237c
Nguyen Thac Hung <115692325+Zn101@users.noreply.github.com>
2025-09-03 21:36:57 +0700

    Update Bai4.tsx

--- src/components/Bai4.tsx
+++ src/components/Bai4.tsx
@@ -1,6 +1,6 @@
 export default function Bai4() {
   return (
-    <button className="bg-[#0ea5e9] hover:bg-[#0369a1] text-white px-4 py-2 rounded-md">
+    <button className="bg-[#0ea5e9] hover:bg-[#0369a1] text-white px-4 py-2 rounded-md transition-colors duration-300 ease-in-out">
       Submit
     </button>
   )
